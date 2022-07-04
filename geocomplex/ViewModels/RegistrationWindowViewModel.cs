215d833fa8765a91a65018b40785e5770798e388
Александр Зайбель <alexzaibel98@mail.ru>
2022-07-04 11:02:27 +0700

    Реализация команды подключения к базе, проверка логина и пароля

--- geocomplex/ViewModels/RegistrationWindowViewModel.cs
+++ geocomplex/ViewModels/RegistrationWindowViewModel.cs
@@ -1,15 +1,21 @@
-﻿using geocomplex.ViewModels.Base;
+﻿using geocomplex.Infrastructure.Commands;
+using geocomplex.ViewModels.Base;
 using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace geocomplex.ViewModels
 {
     internal class RegistrationWindowViewModel:ViewModel
     {
+        private string connstring = @"Server=localhost;Port=5432; User Id=postgres;Password=admin; Database=Geocomplex;"; // Строка подключения
+        private NpgsqlConnection connection;
 
         #region Логин
         /// <summary>
@@ -31,37 +37,61 @@ namespace geocomplex.ViewModels
 
 
 
-        private string connstring = @"Server=localhost;Port=5432; User Id=postgres;Password=admin; Database=Geocomplex;";
 
-        private NpgsqlConnection connection;
-
-
-            #region Подлключение к БД
-
-            //connection = new NpgsqlConnection(connstring);
-            //connection.Open();
-            
-
-            #endregion
-
-              // ищем в базе данных пользователя с такими данными     
-
-                    //string cmd = $"SELECT * FROM user_data WHERE user_login='{TextBoxLogin.Text}'"; // Создаем запрос для вывода 
-                    //NpgsqlCommand createCommand = new NpgsqlCommand(cmd, connection); // ложим запрос в команду и подключение к бд
-                    //createCommand.ExecuteNonQuery();
-
-                    //NpgsqlDataAdapter dataAdp = new NpgsqlDataAdapter(createCommand);
-                    //DataTable dt = new DataTable("user_data"); // В скобках указываем название таблицы
-                    //dataAdp.Fill(dt);
-                    //if (dt.Rows.Count > 0) // если такая запись существует       
-                    //{
-                    //    MainWindow main = new MainWindow();
-                    //    main.Show();
-                    //    connection.Close();
-                    //}
+        /*-------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
+
+        #region Команды
+
+
+        #region Команда Подключения и проверки логина с паролем пользователя
+        /// <summary>
+        ///  Команда Подключения и проверки логина с паролем пользователя
+        /// </summary>
+        public ICommand ConnectionCommand { get; } // Сама команда
+
+        private bool CanConnectionCommandExecute(object p) => true;
+
+        // Действия которые должна выполнить команда
+        private void OnConnectionCommandExcuted(object p)
+        {
+            connection = new NpgsqlConnection(connstring);
+            connection.Open();
+            string cmd = $"SELECT * FROM user_data WHERE user_login='{login}' AND user_password ='{password}'"; // Создаем запрос для вывода 
+            NpgsqlCommand createCommand = new NpgsqlCommand(cmd, connection); // ложим запрос в команду и подключение к бд
+            createCommand.ExecuteNonQuery();
+
+            NpgsqlDataAdapter dataAdp = new NpgsqlDataAdapter(createCommand);
+            DataTable dt = new DataTable("user_data"); // В скобках указываем название таблицы
+            dataAdp.Fill(dt);
+           
+           
+            if (dt.Rows.Count > 0) // если такая запись существует       
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+                connection.Close();
+               
+            }
+        }
+
+        #endregion
+
+
+        #endregion
+
+        
+
+        /*-------------------------------------------------------------------------------------------------------------------------------------------*/
+        public RegistrationWindowViewModel()
+        {
+            ConnectionCommand = new LamdaCommand(OnConnectionCommandExcuted, CanConnectionCommandExecute);
 
         }
+
+
+
+    }
 }
