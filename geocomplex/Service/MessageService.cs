ff062bccbc6905fb4286df4e0acf36ae6cfa89dd
Александр Зайбель <alexzaibel98@mail.ru>
2022-07-12 17:37:41 +0700

    Добавил сервис для вывода информационного сообщения пользователю

---
+++ geocomplex/Service/MessageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace geocomplex.Service
+{
+    public static class MessageService
+    {
+        /// <summary>
+        /// Сообщение пользователю
+        /// </summary>
+        /// <param name="message">текст сообщения</param>
+        public static void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
